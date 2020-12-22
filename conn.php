901085bfe53f1974c78d466633cbc1f6dd2266d3
nishat696 <mithila@pentabd.com>
2020-12-22 17:00:08 +0600

    crud-operation

---
+++ conn.php
@@ -0,0 +1,13 @@
+<?php
+
+$con = mysqli_connect('localhost','root');
+
+mysqli_select_db($con,'crudoperation');
+
+// if($con){
+//     echo "db connected";
+// }else {
+//     echo "db not connected";
+// }
+
+?>
